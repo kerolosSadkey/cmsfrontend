9f3dcd9bda0e34a1b2f1365d3a587ec44c9fbbc8
unknown <sadkey17@gmail.com>
2022-07-18 18:01:32 +0200

    update on some modules

--- src/app/views/layout/sidebar/menu.ts
+++ src/app/views/layout/sidebar/menu.ts
@@ -44,7 +44,7 @@ export const MENU: MenuItem[] = [
     label: 'Calendar',
     icon: 'calendar',
     notify:true,
-    link: '/calendar',
+    link: 'apps/calendar',
     badge: {
       variant: 'primary',
       text: 'Event',
